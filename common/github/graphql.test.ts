16097c1b7e58266f3aa831766de2fea00b8f6304
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-05-24 10:39:33 +0900

    update test token

--- common/github/graphql.test.ts
+++ common/github/graphql.test.ts
@@ -6,7 +6,7 @@ import { getCommitCount, getCommitCountAndId } from './graphql'
 test('get commit count.', async (t) => {
 	process.env.BASE_DATE = '2020-04-01'
 	//No authority token
-	process.env.GITHUB_API_TOKEN = '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'
+	process.env.GITHUB_API_TOKEN = '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'
 	const result = await getCommitCount('hhatto')
 	t.is(result, 2379)
 })
@@ -15,7 +15,7 @@ test('get commit count.', async (t) => {
 test('git commit count and id.', async (t) => {
 	process.env.BASE_DATE = '2020-04-01'
 	//public data only token
-	const token = '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'
+	const token = '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'
 	const result = await getCommitCountAndId(token)
 	t.is(result.githubId, 'Akira-Taniguchi')
 	t.is(result.commitCount, 1944)
