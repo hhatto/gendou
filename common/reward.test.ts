f10556ae2c35c6a8806adffd7d49be66cfbd1467
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-06-03 00:29:50 +0900

    add eslint comment

--- common/reward.test.ts
+++ common/reward.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable prettier/prettier */
 /* eslint-disable functional/no-let */
